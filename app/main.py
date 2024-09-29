4e1221c72399687de41b0f5c81b5f1bf83982bbe
nuggetpluto <korshikov1501@gmail.com>
2024-09-29 16:38:29 +0300

    Структура проекта. Маршруты и модели Pydantic.

---
+++ app/main.py
@@ -0,0 +1,21 @@
+# taskmanager/main.py
+
+from fastapi import FastAPI
+from app.routers import task, user
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Taskmanager"}
+
+
+# Подключение маршрутов
+app.include_router(task.router)
+app.include_router(user.router)
+
+# Базовый маршрут
+
+
+# Запуск: uvicorn main:app --reload
---
+++ app/schemas.py
@@ -0,0 +1,30 @@
+# taskmanager/schemas/schemas.py
+
+from pydantic import BaseModel
+
+
+# Схемы пользователя
+class CreateUser(BaseModel):
+    username: str
+    firstname: str
+    lastname: str
+    age: int
+
+
+class UpdateUser(BaseModel):
+    firstname: str
+    lastname: str
+    age: int
+
+
+# Схемы задачи
+class CreateTask(BaseModel):
+    title: str
+    content: str
+    priority: int
+
+
+class UpdateTask(BaseModel):
+    title: str
+    content: str
+    priority: int
